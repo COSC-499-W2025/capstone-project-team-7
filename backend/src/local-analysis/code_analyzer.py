b8c95fddb8fc5f7873b1ece1b44277dbfaff4846
aaronbanerjee123 <aaronbaner@outlook.com>
2025-10-25 10:10:19 -0700

    scan folder method added and changed name of file

--- backend/src/local-analysis/code_analyzer.py
+++ backend/src/local-analysis/code_analyzer.py
@@ -68,3 +68,55 @@ class CodeAnalyzer:
         print("Analysis complete.")
         
         return self.results
+    
+    def scan_folder(self):
+        """
+        Scan the folder and categorize all code files by language
+        
+        This method will:
+        1. Walk through all the subdirectories
+        2. Find code files based on their extension
+        3. Group them up by language
+        4. Calculate total size
+        """
+        
+        print("Scanning folder...")
+        
+        files_by_language = defaultdict(list)
+        
+        total_size = 0 
+        for root, dirs, files in os.walk(self.folder_path):
+            dirs[:] = [d for d in dirs if d not in [
+                'node_modules',
+                '__pycache__',
+                '.git',
+                'venv',
+                'env',
+                'build',
+                'dist',
+                'target',
+                '.idea',
+                '.vscode',
+                'bin',
+                'obj'
+            ]]
+            
+            for file in files:
+                filepath = Path(root)/file
+        
+                ext = filepath.suffix.lower()
+                
+                if ext in self.LANGUAGE_MAP:
+                    language = self.LANGUAGE_MAP[ext]
+                    size = filepath.stat().st_size
+                    
+                    file_info = {
+                        'path':str(filepath.relative_to(self.folder_path)),
+                        'name':file,
+                        'extension':ext,
+                        'size':size,
+                    }
+                    
+                    files_by_language[language].append(file_info)
+                    total_size += size
+                                        
